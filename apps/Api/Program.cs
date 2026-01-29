28318b05ab630d4da8abf8d2b98e3f8e98b34599
EmilAsk-Dev <carl.emil.ask1@gmail.com>
2026-01-30 00:15:01 +0100

    more deploy stuff

--- apps/Api/Program.cs
+++ apps/Api/Program.cs
@@ -105,6 +105,12 @@ if (app.Environment.IsDevelopment())
     await DbSeeder.SeedAsync(app.Services);
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.Lifetime.ApplicationStarted.Register(() =>
 {
     foreach (var url in app.Urls)
